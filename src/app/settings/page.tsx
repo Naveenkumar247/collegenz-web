5c5f98132b6f9a1dffcf3379de4dafae02d658bc
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-06-26 21:57:10 +0530

    Update page.tsx

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-export default function SettingsDummyPage() {
+
+export default function SettingsPage() {
   return (
     <div className="min-h-screen bg-[#f3f4f6] flex items-center justify-center font-sans">
       <div className="text-center space-y-2 max-w-xs bg-white border p-6 rounded-2xl shadow-sm">
